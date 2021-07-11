d2da12b79ed09868d4fa29c7b65fd5de4ee860f1
bulat <ra.bulat@gmail.com>
2021-07-12 00:22:17 +0500

    server nio main -> runner

--- server/src/main/java/org/example/Runner.java
+++ server/src/main/java/org/example/Runner.java
@@ -2,9 +2,13 @@ package org.example;
 
 import org.example.netty.Server;
 
+import java.io.IOException;
+
 public class Runner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Server();
+
+//        new org.example.nio.Server();
     }
 }
